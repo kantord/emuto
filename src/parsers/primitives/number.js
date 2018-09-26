c38855fec030cdfc2116a8936ff6826d0446d96d
Daniel Kantor <git@daniel-kantor.com>
2018-09-26 20:47:12 +0200

    feat: add number primitives

---
+++ src/parsers/primitives/number.js
@@ -0,0 +1,11 @@
+// @flow
+
+import Parsimmon from 'parsimmon'
+import type {NodeType} from '../../types'
+
+export default Parsimmon.regexp(
+  /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/
+).map((value: string): NodeType => ({
+  type: 'primitive',
+  value
+}))
