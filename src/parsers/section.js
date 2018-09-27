4444d47f1aa04ec729b912ddab9010edec1c5480
Daniel Kantor <git@daniel-kantor.com>
2018-09-27 19:42:34 +0200

    fix(tuple): fix parser

--- src/parsers/section.js
+++ src/parsers/section.js
@@ -1,5 +1,6 @@
 import Parsimmon from 'parsimmon'
 import ValuePropParser from './valueProp'
 import ValueParser from './value'
+import TupleParser from './tuple/tuple'
 
-export default Parsimmon.alt(ValuePropParser, ValueParser)
+export default Parsimmon.alt(ValuePropParser, Parsimmon.alt(TupleParser, ValueParser))
--- src/parsers/tuple/tuple.js
+++ src/parsers/tuple/tuple.js
@@ -1,17 +1,15 @@
 // @flow
 
 import Parsimmon from 'parsimmon'
-import type { NodeType, PrimitiveNodeType } from '../../types'
+import type {NodeType, PrimitiveNodeType} from '../../types'
 
 import PrimitiveParser from '../primitive'
 
-const separator = Parsimmon.seq(
-  Parsimmon.optWhitespace,
-  Parsimmon.string(':'),
-  Parsimmon.optWhitespace
-)
-
-export default Parsimmon.seq(PrimitiveParser, separator, PrimitiveParser).map(
+export default Parsimmon.seq(
+  PrimitiveParser,
+  Parsimmon.regexp(/\s*:\s*/),
+  PrimitiveParser
+).map(
   (children: [PrimitiveNodeType, NodeType, PrimitiveNodeType]): NodeType => ({
     type: 'tuple',
     value: [children[0], children[2]]
