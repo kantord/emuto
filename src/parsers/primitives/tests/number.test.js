4a858fa7d648fd438b67f4049145e7c648114694
Daniel Kantor <git@daniel-kantor.com>
2018-09-28 12:45:08 +0200

    refactor: remove code duplicates

--- src/parsers/primitives/tests/number.test.js
+++ src/parsers/primitives/tests/number.test.js
@@ -1,19 +1,10 @@
 // @flow
 
 import parser from '../number'
+import {testExample} from './tools'
 
 const examples = ['3.14', '-42', '0', '111.111', '0.33']
 
 describe('number primitive parser', () => {
-  examples.forEach((example: string) => {
-    it('parses true', () => {
-      expect(parser.parse(example).status).toBe(true)
-    })
-    it('returns correct value', () => {
-      expect(parser.parse(example).value).toEqual({
-        type: 'primitive',
-        value: example
-      })
-    })
-  })
+  examples.forEach(testExample(parser))
 })
