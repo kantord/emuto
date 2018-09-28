b5b5d7b8aab2ecd0e31184c14a4dfcb533bcb86c
Daniel Kantor <git@daniel-kantor.com>
2018-09-28 11:25:04 +0200

    fix: remove redundant valueProp parser

--- src/parsers/value.js
+++ src/parsers/value.js
@@ -3,13 +3,11 @@ import Parsimmon from 'parsimmon'
 import PrimitiveParser from './primitive'
 import InputParser from './input'
 import InputPropParser from './inputProp'
-import ValuePropParser from './valueProp'
 import ListParser from './list'
 
 export default Parsimmon.alt(
   PrimitiveParser,
   InputPropParser,
   InputParser,
-  ListParser,
-  ValuePropParser
+  ListParser
 )
