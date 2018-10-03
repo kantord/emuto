9e38dad8d4cf72a59fcb36c010d652bfbb7231aa
Daniel Kantor <git@daniel-kantor.com>
2018-10-03 19:36:04 +0200

    feat(projection): support more complex projections

--- src/__tests__/builtins.test.js
+++ src/__tests__/builtins.test.js
@@ -10,5 +10,23 @@ describe('built ins', () => {
     it('handles string', () => {
       expect(projection({foo: 'bar'}, ['foo'])).toEqual('bar')
     })
+    it('handles list of strings', () => {
+      expect(projection({foo: 'bar'}, ['foo', 'baz'])).toEqual(['bar', null])
+    })
+    it('handles negative numbers', () => {
+      expect(projection([1, 2, 3], [-1])).toEqual(3)
+      expect(projection([1, 2, 3], [-2])).toEqual(2)
+      expect(projection([1, 2, 3], [-3])).toEqual(1)
+    })
+    it('handles list of numbers', () => {
+      expect(projection([1, 2, 3], [-1, 0])).toEqual([3, 1])
+    })
+    it('handles empty projection', () => {
+      expect(projection([1, 2, 3], [])).toEqual([])
+    })
+    it('handles falsy list items', () => {
+      expect(projection([0], [0])).toEqual(0)
+      expect(projection([false], [0])).toEqual(false)
+    })
   })
 })
--- src/builtins.js
+++ src/builtins.js
@@ -1,5 +1,28 @@
 // @flow
 
+type ProjectableType = Array<mixed> & {[string]: mixed};
+type ProjectionRuleType = number & string;
+type ProjectionRulesType = Array<ProjectionRuleType>;
+
+const convertUndefined = (value: ?mixed): mixed | null =>
+  value === undefined ? null : value
+
+const handleProjectionItem = (
+  projectable: ProjectableType
+): (ProjectionRuleType => mixed) => (
+  projectionRule: ProjectionRuleType
+): mixed =>
+  Number.isInteger(projectionRule)
+    ? convertUndefined(projectable.slice(projectionRule)[0])
+    : convertUndefined(projectable[projectionRule])
+
+const handleProjection = (
+  projectable: ProjectableType
+): (ProjectionRulesType => Array<mixed> & mixed) => (
+  projectionRules: ProjectionRulesType
+): Array<mixed> & mixed =>
+  projectionRules.map(handleProjectionItem(projectable))
+
 export default {
   objectify: (input: Array<[string, mixed]>): {[string]: mixed} =>
     input.reduce(function (
@@ -10,8 +33,8 @@ export default {
       return a
     },
     {}),
-  projection: (
-    left: Array<mixed> & {[string]: mixed},
-    right: Array<number & string>
-  ): mixed => left[right[0]]
+  projection: (left: ProjectableType, right: ProjectionRulesType): mixed =>
+    right.length === 1
+      ? handleProjection(left)(right)[0]
+      : handleProjection(left)(right)
 }
