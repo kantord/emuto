0cd49c1cf9b2615550526edcd92fa7ac92f5ccb6
Daniel Kantor <git@daniel-kantor.com>
2018-10-04 20:28:05 +0200

    feat(program): better whitespace handling

---
+++ src/__tests__/whitespace.test.js
@@ -0,0 +1,12 @@
+// @flow
+
+import parse from '../parsers/parser'
+const examples = [` 0`, `0 `]
+
+describe('whitespace tests', () => {
+  examples.forEach((example: string): void =>
+    it(`parses '${example.slice(0, 25)}'`, () => {
+      parse(example)
+    })
+  )
+})
