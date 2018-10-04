23e22565d4f18872553a6f7e0ce244e5a327146f
Daniel Kantor <git@daniel-kantor.com>
2018-10-04 20:28:05 +0200

    feat(program): better whitespace handling

--- src/parsers/program.js
+++ src/parsers/program.js
@@ -1,5 +1,12 @@
+// @flow
+
 import Parsimmon from 'parsimmon'
 import TupleParser from './tuple/tuple'
 import PipeParser from './pipe/pipe'
+import type {NodeType} from '../types'
 
-export default Parsimmon.alt(PipeParser, TupleParser)
+export default Parsimmon.seq(
+  Parsimmon.optWhitespace,
+  Parsimmon.alt(PipeParser, TupleParser),
+  Parsimmon.optWhitespace
+).map((value: [mixed, NodeType, mixed]): NodeType => value[1])
