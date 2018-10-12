52559b32ce651fe02bb1325a18bf87b95f9d7cca
Daniel Kantor <git@daniel-kantor.com>
2018-10-12 16:07:41 +0200

    refactor: extract BinaryOperatorParser from MultiplicativeParser

---
+++ src/parsers/abstract/binaryOperator.js
@@ -0,0 +1,33 @@
+// @flow
+
+import P from 'parsimmon'
+
+import type { NodeType, ParserType } from '../../types'
+
+const BinaryOperatorParser = (
+  OperandParser: ParserType,
+  OperatorParser: ParserType
+): ParserType =>
+  P.lazy((): mixed => {
+    const OperatorParser_ = OperatorParser.trim(P.optWhitespace).node(
+      'operand'
+    )
+    return P.seq(
+      OperandParser,
+      P.seq(OperatorParser_, OperandParser).many()
+    ).map(
+      (value: [NodeType, Array<NodeType>]): NodeType =>
+        value[1].length > 0
+          ? {
+            name: 'multiplicative',
+            value: value[1].reduce(
+              (a: Array<NodeType>, b: NodeType): Array<NodeType> =>
+                a.concat(b),
+              [value[0]]
+            )
+          }
+          : value[0]
+    )
+  })
+
+export default BinaryOperatorParser
--- src/parsers/multiplicative.js
+++ src/parsers/multiplicative.js
@@ -2,25 +2,7 @@
 
 import P from 'parsimmon'
 
-import type { NodeType } from '../types'
+import OperandParser from './operand'
+import BinaryOperatorParser from './abstract/binaryOperator'
 
-const ListCoreParser = P.lazy((): mixed => {
-  const OperandParser = require('./operand').default
-  const OperatorParser = P.regexp(/[*/]/)
-    .trim(P.optWhitespace)
-    .node('operand')
-  return P.seq(OperandParser, P.seq(OperatorParser, OperandParser).many()).map(
-    (value: [NodeType, Array<NodeType>]): NodeType =>
-      value[1].length > 0
-        ? {
-          name: 'multiplicative',
-          value: value[1].reduce(
-            (a: Array<NodeType>, b: NodeType): Array<NodeType> => a.concat(b),
-            [value[0]]
-          )
-        }
-        : value[0]
-  )
-})
-
-export default ListCoreParser
+export default BinaryOperatorParser(OperandParser, P.regexp(/[*/]/))
