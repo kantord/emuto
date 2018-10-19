074c87078bd203df3e04599ea4cd053bbda55b3e
Daniel Kantor <git@daniel-kantor.com>
2018-10-20 00:11:34 +0200

    style(builtins): add empty lines between functions

--- src/builtins.js
+++ src/builtins.js
@@ -33,16 +33,20 @@ export default {
       return a
     },
     {}),
+
   projection: (left: ProjectableType, right: ProjectionRulesType): mixed =>
     right.length === 1
       ? handleProjection(left)(right)[0]
       : handleProjection(left)(right),
+
   join: (separator: string): ((Array<string>) => string) => (
     input: Array<string>
   ): string => input.join(separator),
+
   map: (f: mixed => mixed): ((Array<mixed>) => Array<mixed>) => (
     input: Array<mixed>
   ): Array<mixed> => input.map(f),
+
   sortBy: (f: <T>(mixed) => T): ((Array<mixed>) => Array<mixed>) => (
     input: Array<mixed>
   ): Array<mixed> =>
@@ -52,12 +56,15 @@ export default {
         (a: mixed, b: mixed): 1 | 0 | -1 =>
           f(a) < f(b) ? -1 : f(a) > f(b) ? 1 : 0
       ),
+
   filter: (f: mixed => boolean): ((Array<mixed>) => Array<mixed>) => (
     input: Array<mixed>
   ): Array<mixed> => input.filter(f),
+
   get: function (variable: string): mixed {
     return this[variable]
   },
+
   assign: function (
     variable: string,
     value: mixed,
@@ -65,10 +72,14 @@ export default {
   ): {[string]: mixed} {
     return Object.assign({}, context, { [variable]: value })
   },
+
   reverse: (input: Array<mixed>): Array<mixed> => input.slice().reverse(),
+
   reduce: ([f, x]: [(mixed) => mixed, mixed]): ((Array<mixed>) => mixed) => (
     input: Array<mixed>
   ): mixed => input.reduce(f, x),
+
   length: (input: Array<mixed>): number => input.length,
+
   keys: (input: {[string]: mixed}): Array<string> => Object.keys(input)
 }
