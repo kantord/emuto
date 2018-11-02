1ff5d11a6de6dc0da676442e77ddf20f4340bbb3
Daniel Kantor <git@daniel-kantor.com>
2018-11-02 20:50:15 +0100

    feat: implement basic comprehension syntax
    fixes #104

--- src/parsers/listCore.js
+++ src/parsers/listCore.js
@@ -3,11 +3,16 @@
 import P from 'parsimmon'
 import crap from './crap'
 
-import type { ListCoreNodeType, ListCoreValueType } from '../types'
+import type {
+  ListCoreNodeType,
+  ListCoreValueType,
+  TupleNodeType,
+  NodeType
+} from '../types'
 
 const SeparatorParser = P.string(',').trim(crap)
 
-const ListCoreParser = P.lazy((): mixed => {
+const ListCoreLiteralParser = P.lazy((): mixed => {
   const TupleParser = require('./tuple/tuple').default
   const SpreadParser = P.string('...')
     .then(crap)
@@ -24,4 +29,69 @@ const ListCoreParser = P.lazy((): mixed => {
   )
 })
 
-export default ListCoreParser
+const ComprehensionParser = P.lazy((): mixed => {
+  const TupleParser = require('./tuple/tuple').default
+  const ProgramParser = require('./program').default
+  return P.seq(
+    P.string('each')
+      .then(crap)
+      .then(TupleParser),
+    crap.then(P.string('in')).then(crap.then(TupleParser)),
+    crap.then(ProgramParser).atMost(1)
+  ).map(
+    ([left, middle, right]: [
+      TupleNodeType,
+      TupleNodeType,
+      [NodeType]
+    ]): ListCoreNodeType => {
+      const mapPart = {
+        name: 'functionCall',
+        value: {
+          left: {
+            name: 'identifier',
+            value: 'map'
+          },
+          right: {
+            name: 'lambda',
+            value: {
+              variable: 'input',
+              definition: left
+            }
+          }
+        }
+      }
+
+      const extractionPart =
+        right.length === 1
+          ? {
+            name: 'pipe',
+            value: {
+              left: middle,
+              right: right[0]
+            }
+          }
+          : middle
+
+      return {
+        name: 'listCore',
+        value: [
+          {
+            name: 'spread',
+            value: {
+              name: 'parentheses',
+              value: {
+                name: 'pipe',
+                value: {
+                  left: extractionPart,
+                  right: mapPart
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  )
+})
+
+export default P.alt(ComprehensionParser, ListCoreLiteralParser)
