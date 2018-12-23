f60c771b6a64af6a26347d4f7c4cdb2bc3760feb
Daniel Kantor <git@daniel-kantor.com>
2018-12-23 21:35:23 +0100

    refactor(program-parser): remove unnecessary P.alth

--- src/parsers/program.js
+++ src/parsers/program.js
@@ -10,6 +10,6 @@ import crap from './crap'
 
 export default P.seq(
   crap,
-  P.alt(AssignmentParser, PipeParser, P.alt(TernaryParser, TupleParser)),
+  P.alt(AssignmentParser, PipeParser, TernaryParser, TupleParser),
   crap
 ).map((value: [mixed, NodeType, mixed]): NodeType => value[1])
