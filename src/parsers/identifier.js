8587fd503ee6fa3e07c024e69a271418389e1bbe
Daniel Kantor <git@daniel-kantor.com>
2018-12-23 23:50:23 +0100

    refactor(identifier-parser): simplify regular expression

--- src/parsers/identifier.js
+++ src/parsers/identifier.js
@@ -3,9 +3,15 @@
 import P from 'parsimmon'
 import type { IdentifierNodeType } from '../types'
 
-export default P.regexp(
-  /((?!null|false|true|if)[a-zA-Z][0-9a-zA-Z_$]*|(null|false|true|if)[0-9a-zA-Z_$])/
-)
+export default P.regexp(/[a-zA-Z][0-9a-zA-Z_$]*/i)
+  .chain((identifier: string): IdentifierNodeType => {
+    const exceptions = ['null', 'false', 'true', 'if', 'each']
+    if (exceptions.indexOf(identifier) < 0) {
+      return P.of(identifier)
+    } else {
+      return P.fail(`identifier but got keyword ${identifier}`)
+    }
+  })
   .map((value: string): IdentifierNodeType => ({
     name: 'identifier',
     value
