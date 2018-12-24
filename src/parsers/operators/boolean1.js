18bd024208d720a679172889fd2c389ec3d8f304
Daniel Kantor <git@daniel-kantor.com>
2018-12-25 00:22:02 +0100

    refactor: merge AdditiveParser and Boolean1Parser

--- src/parsers/operators/boolean1.js
+++ src/parsers/operators/boolean1.js
@@ -2,9 +2,20 @@
 
 import P from 'parsimmon'
 
-import AdditiveParser from './additive'
 import BinaryOperatorParser from '../abstract/binaryOperator'
+import UnaryOperatorParser from './unaryOperator'
 
+const MultiplicativeParser = BinaryOperatorParser(
+  P.alt(UnaryOperatorParser),
+  P.regexp(/[*/%]/),
+  'multiplicative'
+)
+
+const AdditiveParser = BinaryOperatorParser(
+  MultiplicativeParser,
+  P.regexp(/[+-]/),
+  'additive'
+)
 export default BinaryOperatorParser(
   AdditiveParser,
   P.regexp(/[<>]=?/),
