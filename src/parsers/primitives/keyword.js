416e2fa200ff2c40d90f6417d910fa35274027d5
Daniel Kantor <git@daniel-kantor.com>
2018-12-24 09:17:07 +0100

    perf(keyword-primitive): use regexp to parse

--- src/parsers/primitives/keyword.js
+++ src/parsers/primitives/keyword.js
@@ -3,13 +3,7 @@
 import type { NodeType } from '../../types'
 import P from 'parsimmon'
 
-const TrueParser = P.string('true')
-const FalseParser = P.string('false')
-const NullParser = P.string('null')
-
-export default P.alt(TrueParser, FalseParser, NullParser).map(
-  (value: string): NodeType => ({
-    name: 'primitive',
-    value
-  })
-)
+export default P.regex(/(null|true|false)/).map((value: string): NodeType => ({
+  name: 'primitive',
+  value
+}))
