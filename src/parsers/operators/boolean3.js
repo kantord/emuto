37c461933580def4a4e0798f129b0052f1b6f874
Daniel Kantor <git@daniel-kantor.com>
2018-12-25 00:45:49 +0100

    refactor: structure parsers as an operator precedence table

--- src/parsers/operators/boolean3.js
+++ src/parsers/operators/boolean3.js
@@ -5,33 +5,28 @@ import P from 'parsimmon'
 import BinaryOperatorParser from '../abstract/binaryOperator'
 import UnaryOperatorParser from './unaryOperator'
 
-const MultiplicativeParser = BinaryOperatorParser(
-  P.alt(UnaryOperatorParser),
-  P.regexp(/[*/%]/),
-  'multiplicative'
-)
+import type { ParserType } from '../../types'
 
-const AdditiveParser = BinaryOperatorParser(
-  MultiplicativeParser,
-  P.regexp(/[+-]/),
-  'additive'
-)
+const processBoolean2 = (x: string): string => x + '='
+const processBoolean3 = (x: string): string =>
+  x === 'or' ? '||' : x === 'and' ? '&&' : x
 
-const Boolean1 = BinaryOperatorParser(
-  AdditiveParser,
-  P.regexp(/[<>]=?/),
-  'boolean1'
-)
-const Boolean2 = BinaryOperatorParser(
-  Boolean1,
-  P.regexp(/[=!]=/).map((x: string): string => x + '='),
-  'boolean2'
-)
+const operatorPrecedenceTable = [
+  ['unary', UnaryOperatorParser],
+  ['multiplicative', P.regexp(/[*/%]/)],
+  ['additive', P.regexp(/[+-]/)],
+  ['boolean1', P.regexp(/[<>]=?/)],
+  ['boolean2', P.regexp(/[=!]=/).map(processBoolean2)],
+  ['boolean3', P.regexp(/(\|\||&&|or|and)/).map(processBoolean3)]
+]
 
-export default BinaryOperatorParser(
-  Boolean2,
-  P.regexp(/(\|\||&&|or|and)/).map(
-    (x: string): string => (x === 'or' ? '||' : x === 'and' ? '&&' : x)
-  ),
-  'boolean3'
-)
+export default operatorPrecedenceTable
+  .slice(1)
+  .reduce(
+    (
+      ParentParser: ParserType,
+      [parserDecription, Parser]: [string, ParserType]
+    ): ParserType =>
+      BinaryOperatorParser(ParentParser, Parser, parserDecription),
+    operatorPrecedenceTable[0][1]
+  )
