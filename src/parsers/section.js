e1736c35e78d28e5f913e95e3be569a051ad599b
Daniel Kantor <git@daniel-kantor.com>
2018-12-24 10:03:25 +0100

    perf(section-parser): balance performance

--- src/parsers/section.js
+++ src/parsers/section.js
@@ -2,7 +2,4 @@ import P from 'parsimmon'
 import FunctionCallParser from './functionCall'
 import Boolean3Parser from './operands/boolean3'
 
-export default P.alt(
-  FunctionCallParser,
-  Boolean3Parser
-)
+export default P.alt(Boolean3Parser, FunctionCallParser)
