079b64bc4fb064abcc17fa231e439393df7eb5ed
Daniel Kantor <git@daniel-kantor.com>
2018-12-29 18:17:36 +0100

    refactor(primitive): merge primitive parsers into a single file

--- src/parsers/primitives/__tests__/primitives.test.js
+++ src/parsers/primitives/__tests__/primitives.test.js
@@ -23,4 +23,26 @@ const examples = [...stringExamples, ...numberExamples]
 
 describe('primitive parser', () => {
   examples.forEach(testExample(parser))
+
+  it('parses true', () => {
+    expect(parser.parse('true').status).toBe(true)
+  })
+  it('parses false', () => {
+    expect(parser.parse('false').status).toBe(true)
+  })
+  it('returns correct value', () => {
+    expect(parser.parse('false').value).toEqual({
+      name: 'primitive',
+      value: 'false'
+    })
+  })
+  it('parses null', () => {
+    expect(parser.parse('null').status).toBe(true)
+  })
+  it('returns correct value', () => {
+    expect(parser.parse('null').value).toEqual({
+      name: 'primitive',
+      value: 'null'
+    })
+  })
 })
--- src/parsers/primitives/primitive.js
+++ src/parsers/primitives/primitive.js
@@ -1,7 +1,29 @@
-import P from 'parsimmon'
+// @flow
+/* eslint no-control-regex: 0 */
+/* eslint no-useless-escape: 0 */
 
-import KeywordParser from './keyword'
-import StringParser from './string'
-import NumberParser from './number'
+import P from 'parsimmon'
+import type { NodeType } from '../../types'
+
+const KeywordParser = P.regex(/(null|true|false)/).map(
+  (value: string): NodeType => ({
+    name: 'primitive',
+    value
+  })
+)
+
+const StringParser = P.regexp(
+  /("(((?=\\)\\(["\\\/bfnrt]|u[0-9a-fA-F]{4}))|[^"\\\0-\x1F\x7F]+)*")/
+).map((value: string): NodeType => ({
+  name: 'primitive',
+  value
+}))
+
+const NumberParser = P.regexp(/(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/).map(
+  (value: string): NodeType => ({
+    name: 'primitive',
+    value
+  })
+)
 
 export default P.alt(KeywordParser, StringParser, NumberParser)
