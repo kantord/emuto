77015933cc2926332e97ac24270b75e127b49a77
Daniel Kantor <git@daniel-kantor.com>
2019-01-04 10:46:21 +0100

    test(pipe-parser): test single-item pipeline

--- src/parsers/__tests__/pipe.test.js
+++ src/parsers/__tests__/pipe.test.js
@@ -2,7 +2,7 @@
 
 import parser from '../pipe'
 
-const codeExamples = ['$ | false', '.asd|.fgh']
+const codeExamples = ['$ | false', '.asd|.fgh', '3']
 
 describe('pipe parser', () => {
   it('returns correct value', () => {
@@ -17,6 +17,12 @@ describe('pipe parser', () => {
   codeExamples.forEach((codeExample: string) => {
     it('parses code example', () => {
       expect(parser.parse(codeExample).status).toBe(true)
+    })
+  })
+  it('returns correct value 2', () => {
+    expect(parser.parse(codeExamples[2]).value).toMatchObject({
+      name: 'pipe',
+      value: [{ name: 'primitive', value: '3' }]
     })
   })
 })
