c5ce0c7bd8add8433f3283ceb06256d8ae8926d6
Daniel Kantor <git@daniel-kantor.com>
2019-01-18 16:09:27 +0100

    feat: implement operators as functions

--- src/generators/__tests__/unaryOperator.test.js
+++ src/generators/__tests__/unaryOperator.test.js
@@ -15,6 +15,40 @@ describe('unaryOperator generator', () => {
           }
         }
       })
-    ).toEqual('(-(4))')
+    ).toEqual('(_.__negateNumber__(4))')
+  })
+
+  it('generates correct code', () => {
+    expect(
+      unaryOperator({
+        name: 'unaryOperator',
+        value: {
+          operator: {
+            value: '+'
+          },
+          operand: {
+            name: 'primitive',
+            value: '4'
+          }
+        }
+      })
+    ).toEqual('(_.__id__(4))')
+  })
+
+  it('generates correct code', () => {
+    expect(
+      unaryOperator({
+        name: 'unaryOperator',
+        value: {
+          operator: {
+            value: '!'
+          },
+          operand: {
+            name: 'primitive',
+            value: '4'
+          }
+        }
+      })
+    ).toEqual('(_.__not__(4))')
   })
 })
