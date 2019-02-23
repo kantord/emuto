40f4eb240bf35c2a05cc09d709a3fe2fc5978da2
Dániel Kántor <git@daniel-kantor.com>
2019-02-23 23:10:58 +0100

    feat: implement object projection
    fixes #242

--- src/generators/generator.js
+++ src/generators/generator.js
@@ -1,63 +1,63 @@
 // @flow
 
-import primitive from './primitive';
-import tuple from './tuple';
-import inputProp from './inputProp';
-import valueProp from './valueProp';
-import list from './list';
-import parentheses from './parentheses';
-import object from './object';
-import projection from './projection';
-import objectProjection from './objectProjection';
-import pipe from './pipe';
-import functionCall from './functionCall';
-import binaryOperator from './binaryOperator';
-import unaryOperator from './unaryOperator';
-import ternary from './ternary';
-import assignment from './assignment';
-import lambda from './lambda';
-import variable from './variable';
-import type {NodeType, GeneratedCodeType} from '../types';
+import primitive from './primitive'
+import tuple from './tuple'
+import inputProp from './inputProp'
+import valueProp from './valueProp'
+import list from './list'
+import parentheses from './parentheses'
+import object from './object'
+import projection from './projection'
+import objectProjection from './objectProjection'
+import pipe from './pipe'
+import functionCall from './functionCall'
+import binaryOperator from './binaryOperator'
+import unaryOperator from './unaryOperator'
+import ternary from './ternary'
+import assignment from './assignment'
+import lambda from './lambda'
+import variable from './variable'
+import type { NodeType, GeneratedCodeType } from '../types'
 
 const Generator = (node: NodeType): GeneratedCodeType => {
   switch (node.name) {
     case 'primitive':
-      return primitive(node);
+      return primitive(node)
     case 'variable':
-      return variable(node);
+      return variable(node)
     case 'tuple':
-      return tuple(node);
+      return tuple(node)
     case 'inputProp':
-      return inputProp(node);
+      return inputProp(node)
     case 'list':
-      return list(node);
+      return list(node)
     case 'pipe':
-      return pipe(node);
+      return pipe(node)
     case 'valueProp':
-      return valueProp(Generator)(node);
+      return valueProp(Generator)(node)
     case 'object':
-      return object(node);
+      return object(node)
     case 'parentheses':
-      return parentheses(node);
+      return parentheses(node)
     case 'projection':
-      return projection(Generator)(node);
+      return projection(Generator)(node)
     case 'objectProjection':
-      return objectProjection(Generator)(node);
+      return objectProjection(Generator)(node)
     case 'functionCall':
-      return functionCall(Generator)(node);
+      return functionCall(Generator)(node)
     case 'lambda':
-      return lambda(node);
+      return lambda(node)
     case 'binaryOperation':
-      return binaryOperator(node);
+      return binaryOperator(node)
     case 'unaryOperation':
-      return unaryOperator(node);
+      return unaryOperator(node)
     case 'assignment':
-      return assignment(Generator)(node);
+      return assignment(Generator)(node)
     case 'ternary':
-      return ternary(node);
+      return ternary(node)
     default:
-      throw new Error(`Unknown node name '${node.name}'`);
+      throw new Error(`Unknown node name '${node.name}'`)
   }
-};
+}
 
-export default Generator;
+export default Generator
