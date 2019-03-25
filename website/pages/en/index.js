ed108036c097b3edc4c71a3764fbd4e3a2320a32
Dániel Kántor <git@daniel-kantor.com>
2019-03-25 20:39:07 +0100

    docs: add documentation about supporting different file formats

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,43 +5,43 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require('react')
 
-const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require('../../core/CompLibrary.js')
 
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
+const MarkdownBlock = CompLibrary.MarkdownBlock /* Used to read markdown */
+const Container = CompLibrary.Container
+const GridBlock = CompLibrary.GridBlock
 
-const siteConfig = require(`${process.cwd()}/siteConfig.js`);
+const siteConfig = require(`${process.cwd()}/siteConfig.js`)
 
-function imgUrl(img) {
-  return `${siteConfig.baseUrl}img/${img}`;
+function imgUrl (img) {
+  return `${siteConfig.baseUrl}img/${img}`
 }
 
-function docUrl(doc, language) {
-  return `${siteConfig.baseUrl}docs/${language ? `${language}/` : ''}${doc}`;
+function docUrl (doc, language) {
+  return `${siteConfig.baseUrl}docs/${language ? `${language}/` : ''}${doc}`
 }
 
-function pageUrl(page, language) {
-  return siteConfig.baseUrl + (language ? `${language}/` : '') + page;
+function pageUrl (page, language) {
+  return siteConfig.baseUrl + (language ? `${language}/` : '') + page
 }
 
 class Button extends React.Component {
-  render() {
+  render () {
     return (
       <div className="pluginWrapper buttonWrapper">
         <a className="button" href={this.props.href} target={this.props.target}>
           {this.props.children}
         </a>
       </div>
-    );
+    )
   }
 }
 
 Button.defaultProps = {
-  target: '_self',
-};
+  target: '_self'
+}
 
 const SplashContainer = props => (
   <div className="homeContainer">
@@ -49,19 +49,19 @@ const SplashContainer = props => (
       <div className="wrapper homeWrapper">{props.children}</div>
     </div>
   </div>
-);
+)
 
 const Logo = props => (
   <div>
     <img className="customLogo" src={props.img_src} alt={props.img_alt} />
   </div>
-);
+)
 
 const ProjectTitle = () => (
   <h2 className="projectTitle">
     <Logo img_src={imgUrl('emuto.svg')} img_alt={siteConfig.title} />
   </h2>
-);
+)
 
 const PromoSection = props => (
   <div className="section promoSection">
@@ -69,27 +69,23 @@ const PromoSection = props => (
       <div className="pluginRowBlock">{props.children}</div>
     </div>
   </div>
-);
+)
 
 class HomeSplash extends React.Component {
-  render() {
-    const language = this.props.language || '';
+  render () {
+    const language = this.props.language || ''
     return (
       <SplashContainer>
         <div className="inner">
           <ProjectTitle />
           <PromoSection>
-            <Button href={docUrl('try_emuto', language)}>
-              Try it out
-            </Button>
-            <Button href={docUrl('tutorial', language)}>
-              Get started
-            </Button>
+            <Button href={docUrl('try_emuto', language)}>Try it out</Button>
+            <Button href={docUrl('tutorial', language)}>Get started</Button>
             <Button href={siteConfig.repoUrl}>GitHub</Button>
           </PromoSection>
         </div>
       </SplashContainer>
-    );
+    )
   }
 }
 
@@ -97,25 +93,28 @@ const Block = props => (
   <Container
     padding={['bottom', 'top']}
     id={props.id}
-    background={props.background}>
+    background={props.background}
+  >
     <GridBlock align="center" contents={props.children} layout={props.layout} />
   </Container>
-);
+)
 
 const Features = () => (
   <Block layout="twoColumn">
     {[
       {
-        title: 'Transform JSON data',
-        content: 'Restructure, transform, and manipulate JSON files effortlessly',
+        title: 'Transform and query data structures',
+        content:
+          'Restructure, transform, and manipulate data structures effortlessly'
+      },
+      {
+        title: 'Supports multiple file formats',
+        content: 'plain text, JSON, csv, tsv, dsv'
       },
       {
         title: 'Inspired by jq and GraphQL',
-        content: "[Compare emuto to jq and GraphQL](/emuto/docs/comparison_with_other_languages)",
-      },
-      {
-        title: 'Interface between applications',
-        content: "Emuto can be used as an interface between applications that take different data structures",
+        content:
+          '[Compare emuto to jq and GraphQL](/emuto/docs/comparison_with_other_languages)'
       },
       {
         title: 'Consume APIs in the command line',
@@ -126,40 +125,43 @@ const Features = () => (
 
 Setup:
 \`yarn add --dev emuto emuto-loader\``,
-        title: `Compile to JavaScript`,
+        title: `Compile to JavaScript`
       },
       {
         content: `Combine emuto with well-known utilities such as \`curl\`, \`awk\`
 
 Setup:
 \`yarn global add emuto emuto-cli\``,
-        title: `Command line tool`,
-      },
+        title: `Command line tool`
+      }
     ]}
   </Block>
-);
+)
 
 const FeatureCallout = () => (
   <div
     className="productShowcaseSection paddingBottom"
-    style={{textAlign: 'center'}}>
+    style={{ textAlign: 'center' }}
+  >
     <h2>Flexible</h2>
     <MarkdownBlock>
       Convert, transform, filter, slice JSON files in node or in the browser
     </MarkdownBlock>
   </div>
-);
+)
 
 const Showcase = props => {
   if ((siteConfig.users || []).length === 0) {
-    return null;
+    return null
   }
 
-  const showcase = siteConfig.users.filter(user => user.pinned).map(user => (
-    <a href={user.infoLink} key={user.infoLink}>
-      <img src={user.image} alt={user.caption} title={user.caption} />
-    </a>
-  ));
+  const showcase = siteConfig.users
+    .filter(user => user.pinned)
+    .map(user => (
+      <a href={user.infoLink} key={user.infoLink}>
+        <img src={user.image} alt={user.caption} title={user.caption} />
+      </a>
+    ))
 
   return (
     <div className="productShowcaseSection paddingBottom">
@@ -172,28 +174,36 @@ const Showcase = props => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
 class Index extends React.Component {
-  render() {
-    const language = this.props.language || '';
+  render () {
+    const language = this.props.language || ''
 
     return (
       <div>
         <HomeSplash language={language} />
         <div className="mainContainer">
-          <img src={imgUrl('demo.gif')} alt="" style={{margin: "auto", display: "block"}} />
+          <img
+            src={imgUrl('demo.gif')}
+            alt=""
+            style={{ margin: 'auto', display: 'block' }}
+          />
           <Features />
           <FeatureCallout />
         </div>
-        <style dangerouslySetInnerHTML={{__html:`
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
             .blockContent a {
                 text-decoration: underline;
-            }`}} />
+            }`
+          }}
+        />
       </div>
-    );
+    )
   }
 }
 
-module.exports = Index;
+module.exports = Index
